3b5af26db954fe1c9760c9e0afc4275ea24b676a
Hasham <>
2014-02-11 17:35:08 +0100

    initial project build

---
+++ src/halqasoftware/HalqaSoftware.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package halqasoftware;
+
+/**
+ *
+ * @author -Admin04-
+ */
+public class HalqaSoftware {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
